cd635a21de5ccb2ec1977a53be11b46a6b509cc3
elenkamir <lenamir96@rambler.ru>
2017-02-08 17:49:12 +0900

    Сложение

---
+++ вычисления.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class sdfg {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a, b;
+        System.out.println("¬ведите а: ");
+        a = in.nextInt();
+        System.out.println("¬ведите b: ");
+        b = in.nextInt();
+        System.out.println(sum(a,b));
+    }
+    public static int sum(int a, int b){
+        return a+b;
+    }
+
+}
